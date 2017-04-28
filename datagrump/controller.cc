51e175faa760be2c5de3be4263b8b029bb907eb7
Matthew Denton <mdenton@cs.stanford.edu>
2017-04-28 16:28:42 -0700

    Revert "AIMD"
    This reverts commit 36ae85e9cfe146f80a2f52a4211c0ae0e72c64ef.

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -5,18 +5,16 @@
 
 using namespace std;
 
-
-  
 /* Default constructor */
 Controller::Controller( const bool debug )
-  : debug_( debug ), the_window_size(1), seq_number_sent(0), seq_number_acked(0),  num_acks(0)
+  : debug_( debug )
 {}
 
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
   /* Default: fixed window size of 100 outstanding datagrams */
-  
+  unsigned int the_window_size = 50;
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
@@ -33,7 +31,6 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
                                     /* in milliseconds */
 {
   /* Default: take no action */
-  seq_number_sent = sequence_number;
 
   if ( debug_ ) {
     cerr << "At time " << send_timestamp
@@ -51,12 +48,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  if (seq_number_sent <= sequence_number_acked && seq_number_sent + the_window_size > sequence_number_acked) {
-    seq_number_acked = sequence_number_acked;
-    num_acks++;
-  }
-  if (num_acks == the_window_size)
-    the_window_size++;
+  /* Default: take no action */
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
@@ -66,10 +58,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 	 << endl;
   }
 }
-void Controller::notify_timeout( void ) {
-  the_window_size = (the_window_size > 1) ? the_window_size >> 1 : 1;
-  num_acks = 0;
-}
+
 /* How long to wait (in milliseconds) if there are no acks
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
--- datagrump/controller.hh
+++ datagrump/controller.hh
@@ -9,10 +9,7 @@ class Controller
 {
 private:
   bool debug_; /* Enables debugging output */
-  unsigned int the_window_size;
-  uint64_t seq_number_sent;
-  uint64_t seq_number_acked;
-  uint64_t num_acks;
+
   /* Add member variables here */
 
 public:
@@ -39,8 +36,6 @@ public:
   /* How long to wait (in milliseconds) if there are no acks
      before sending one more datagram */
   unsigned int timeout_ms( void );
-
-  void notify_timeout( void );
 };
 
 #endif
