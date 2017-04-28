58979fd75e604377445b26ac67f0b3bac407e600
Matthew Denton <mdenton@cs.stanford.edu>
2017-04-28 15:28:29 -0700

    Added bandwidth slope estimation

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -128,7 +128,7 @@ static T calcMinInTimeWindow(uint64_t time_window, uint64_t curr_time,
 
 // credit to http://stackoverflow.com/questions/18939869/how-to-get-the-slope-of-a-linear-regression-line-using-c
 double Controller::bw_slope() {
-  cerr << "Last bw estimate timestamp: " << curr_bw_estimate_timestamp << endl;
+  // cerr << "Last bw estimate timestamp: " << curr_bw_estimate_timestamp << endl;
   auto last_bw_estimate = bw_estimates.lower_bound(curr_bw_estimate_timestamp);
   auto end = bw_estimates.end();
 
@@ -145,18 +145,53 @@ double Controller::bw_slope() {
   avgX /= n;
   avgY /= n;
 
-  cerr << "AvgX: " << avgX << ", avgY: " << avgY << endl;
+  // cerr << "AvgX: " << avgX << ", avgY: " << avgY << endl;
 
   double numerator = 0.0;
   double denominator = 0.0;
 
   for (auto it = last_bw_estimate; it != end; ++it) {
-    numerator += (it->first - avgX) * (it->second - avgY);
-    denominator += (it->first - avgX) * (it->first - avgX);
+    numerator += ((it->first - curr_bw_estimate_timestamp) - avgX) * (it->second - avgY);
+    denominator += ((it->first - curr_bw_estimate_timestamp) - avgX) * ((it->first - curr_bw_estimate_timestamp) - avgX);
   }
 
   if (denominator == 0) {
     return 0.0; // don't know slope yet
+  }
+
+  if (numerator / denominator < -0.2) {
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
+    // cerr << "FOUND " << (numerator / denominator) << endl << endl << endl << endl;
   }
 
   return numerator / denominator;
@@ -178,13 +213,13 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   packet_ack_sent_time[sequence_number_acked] = recv_timestamp_acked;
 
 
-  cerr << "--------------------------------------------" << endl;
-  cerr << "Packet sent time: " << send_timestamp_acked << endl;
+  // cerr << "--------------------------------------------" << endl;
+  // cerr << "Packet sent time: " << send_timestamp_acked << endl;
 
 
   uint64_t rtt_est = timestamp_ack_received -
                      send_timestamp_acked; // convert to ms
-  cerr << "RTT_est (ms): " << rtt_est << endl;
+  // cerr << "RTT_est (ms): " << rtt_est << endl;
 
   rtt_estimates.emplace(timestamp_ack_received, rtt_est);
   // TODO change 200 here
@@ -200,8 +235,8 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   double bw_est = (delivered_bytes - packet_delivered[sequence_number_acked]) /
                   ((double)rtt_est);
   bw_estimates.emplace(timestamp_ack_received, bw_est);
-  cerr << "Delivered between ACK and now: " << (delivered_bytes - packet_delivered[sequence_number_acked]) << endl;
-  cerr << "BW_est (Mbps): " << bw_est * 8 / 1000.0 << endl;
+  // cerr << "Delivered between ACK and now: " << (delivered_bytes - packet_delivered[sequence_number_acked]) << endl;
+  // cerr << "BW_est (Mbps): " << bw_est * 8 / 1000.0 << endl;
 
 
   // TODO REMOVE silly slope calculation doesn't work very well
@@ -227,18 +262,18 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   curr_bw_slope_estimate = bw_slope();
 
 
-  cerr << "Current estimation of slope: " << curr_bw_slope_estimate << endl;
-  cerr << "Curr bw estimate (Mbps): " << (curr_bw_estimate * 8 / 1000.0) << endl;
-  cerr << "Curr rtt estimate (ms): " << (curr_rtt_estimate) << endl;
+  // cerr << "Current estimation of slope: " << curr_bw_slope_estimate << endl;
+  // cerr << "Curr bw estimate (Mbps): " << (curr_bw_estimate * 8 / 1000.0) << endl;
+  // cerr << "Curr rtt estimate (ms): " << (curr_rtt_estimate) << endl;
 
 
 
   if ( debug_ || true) {
-    cerr << "At time " << timestamp_ack_received
-         << " received ack for datagram " << sequence_number_acked
-         << " (send @ time " << send_timestamp_acked
-         << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
-         << endl;
+    // cerr << "At time " << timestamp_ack_received
+    //      << " received ack for datagram " << sequence_number_acked
+    //      << " (send @ time " << send_timestamp_acked
+    //      << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
+    //      << endl;
 
   }
 
@@ -248,7 +283,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   // screw with pacing_gain
 
   if (start_up) {
-    cerr << "Received ACK in startup!" << endl;
+    // cerr << "Received ACK in startup!" << endl;
     if (timestamp_ack_received >= time_to_change_phase) {
       time_to_change_phase = timestamp_ack_received + curr_rtt_estimate;
 
@@ -275,7 +310,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   }
   else {
     if (start_up_drain) {
-      cerr << "Received ACK in startup drain! RTT_est = " << rtt_est << ", curr rtt estimate = " << curr_rtt_estimate << endl;
+      // cerr << "Received ACK in startup drain! RTT_est = " << rtt_est << ", curr rtt estimate = " << curr_rtt_estimate << endl;
       // stay in drain until RTT=RTTprop, then reset pacing gain and time_to_change_phase
       if (1.1 * curr_rtt_estimate > rtt_est) {
         start_up_drain = false;
@@ -301,7 +336,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 
       // if rtt becomes too long, start draining immediately (fallback for everything else)
       if (rtt_est > curr_rtt_estimate * 2.0 && phase != 1) {
-        cerr << "immediately start draining!!!!!!!!!! RTT very high!" << endl;
+        // cerr << "immediately start draining!!!!!!!!!! RTT very high!" << endl;
         phase = 1;
         time_to_change_phase = timestamp_ack_received + curr_rtt_estimate;
         extra_gain = 1.0;
@@ -310,20 +345,20 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 
       // do not transition back to probing phase if our bandwidth is decreasing
       // TODO FIXME together with the pacing_gain thing with low slope below, we may never be able to recover from the drop in pacing_gain
-      if (phase > 6 && curr_bw_slope_estimate > -5.0)  {// TODO constant
+      if (phase > 6)  {// TODO constant
         phase = 0;
       }
 
       if (phase == 0) {
         phase = 0;
         // pacing_gain goes up
-        cout << "Extra gain: " << extra_gain << endl;
+        // cout << "Extra gain: " << extra_gain << endl;
         pacing_gain = extra_gain * 1.25; // TODO constant
         cwnd_gain = extra_gain * 1.5;
       } else if (phase == 1) {
         // TODO may not want to do if bandwidth estimate increases
         pacing_gain = 0.75;
-        cwnd_gain = 0.8;
+        cwnd_gain = 0.9;
       } else {
         pacing_gain = 1;
         cwnd_gain = 1.25;
@@ -351,10 +386,6 @@ double Controller::get_bw_estimate()
 double Controller::get_pacing_gain()
 {
   std::lock_guard<std::mutex> lock(global_lock);
-
-  // TODO FIXME need to shift on major shifts of slope, especially downward slop
-  // TODO FIXME current starting values 320 and curr_bw_estimate really make no sense,
-  // currently just lowers pacing_gain if slope is negative.
   if (!start_up && !start_up_drain && phase > 1 && curr_bw_slope_estimate < 1) {
     // based on slope
     uint64_t time_since_bw_estimate = (timestamp_ms() - curr_bw_estimate_timestamp);
